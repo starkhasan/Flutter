cf3c6458b8fa8dd6a3d2ec33a7f98f0a13702b7f
starkhasan <alihasan226@gmail.com>
2021-05-28 19:01:02 +0530

    Add Scrolling in VirtualChar

--- hello_flutter/lib/ui/VirtualChatting.dart
+++ hello_flutter/lib/ui/VirtualChatting.dart
@@ -1,7 +1,8 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
 
 class VirtualChatting extends StatefulWidget {
   @override
@@ -14,6 +15,9 @@ class _VirtualChattingState extends State<VirtualChatting> {
   var myRefReceiver;
   var database;
   var _contMessage = TextEditingController();
+  var snapShot;
+  var sender = 'Shahid';
+  var _scrollController = ScrollController();
 
   @override
   void initState() {
@@ -32,46 +36,85 @@ class _VirtualChattingState extends State<VirtualChatting> {
         title: Text('Chat')
       ),
       body: Container(
+        color: Colors.grey[200],
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Expanded(
               child: Container(
-                color: Colors.red,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 2),
                 child: StreamBuilder(
-                  stream: myRefSender.onValue,
+                  stream: myRefSender.child('ali-shahid').onValue,
                   builder: (context,snapshot){
-                    myRefSender.onValue.listen((event) {
-                      var snapshot = event.snapshot;
-                      var message = snapshot.value['ali-shahid'].keys;
-                      for (var item in message) {
-                        print('= $item');
-                      }
-                      //String value = snapshot.value['Relay1']['Data'];
-                      //print(message);
-                    });
-                    return Container(
-                      child: Text('Sender')
-                    );
-                  },
+                    if(snapshot.hasData){
+                      var notes = snapshot.data.snapshot.value;
+                      return ListView.builder(
+                        controller: _scrollController,
+                        shrinkWrap: true,
+                        physics: BouncingScrollPhysics(),
+                        itemCount: notes.length,
+                        itemBuilder: (context,index){
+                          var key = notes.keys.elementAt(index);
+                          Timer(Duration(milliseconds: 100), () => _scrollController.animateTo(_scrollController.position.maxScrollExtent, duration: Duration(milliseconds: 100), curve: Curves.easeOut));
+                          return Align(
+                            alignment: notes[key]['sender'] == sender ? Alignment.centerRight : Alignment.centerLeft,
+                              child: Container(
+                                margin: EdgeInsets.all(2),
+                                padding: EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  color: notes[key]['sender'] == sender ? Colors.green[100] : Colors.white,
+                                  borderRadius: BorderRadius.all(Radius.circular(8))
+                                ),
+                              child: Text(
+                                notes[key]['message'],
+                                style: TextStyle(color: Colors.black)
+                              )
+                            )
+                          );
+                        }
+                      );                      
+                    }
+                    return Container(child: Center(child:Text('No Message')));
+                  }
                 ),
               ),
             ),
             Container(
               padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              color: Colors.transparent,
               child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Flexible(
-                    child: TextField(
-                      controller: _contMessage,
-                      keyboardType: TextInputType.text,
-                      textInputAction: TextInputAction.send
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                      child: TextField(
+                        controller: _contMessage,
+                        keyboardType: TextInputType.multiline,
+                        textInputAction: TextInputAction.send,
+                        style: TextStyle(color: Colors.black),
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type a message',
+                          hintStyle: TextStyle(color: Colors.grey)
+                        ),
+                        onSubmitted: (message) => sendMessage(),
+                      )
                     )
                   ),
-                  Container(
-                    child: InkWell(
-                      onTap: () => sendMessage(),
-                      child: Icon(Icons.send)
+                  SizedBox(width: MediaQuery.of(context).size.width * 0.01),
+                  InkWell(
+                    onTap: () => sendMessage(),
+                    child: Container(
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.pink
+                      ),
+                      child: Icon(Icons.send,color: Colors.white)
                     )
                   )
                 ]
@@ -84,13 +127,16 @@ class _VirtualChattingState extends State<VirtualChatting> {
   }
 
   sendMessage(){
-    myRefSender.child('ali-shahid').push().set({
-      'message' : 'Hello Howo Are YOu',
-      'sender': 'Ali',
-    });
-    myRefReceiver.child('shahid-ali').push().set({
-      'message' : 'Hello Howo Are YOu',
-      'sender': 'Ali',
-    });
+    if(_contMessage.text.isNotEmpty){
+      myRefSender.child('ali-shahid').push().set({
+        'message' : _contMessage.text.toString(),
+        'sender': sender,
+      });
+      myRefReceiver.child('shahid-ali').push().set({
+        'message' : _contMessage.text.toString(),
+        'sender': sender,
+      });
+    }
+    _contMessage.clear();
   }
 }
