99a50b1f7ed6d539137fe55168c9dc640db629d4
starkhasan <alihasan226@gmail.com>
2021-06-30 11:00:24 +0530

    Delete Useless Api Calling Method

--- covid_info/lib/model/Api.dart
+++ covid_info/lib/model/Api.dart
@@ -1,60 +1,6 @@
 import 'package:http/http.dart' as http;
 
 class Api {
-  static Future<String> globalSummary() async {
-    Map<String, String> headers1 = {'Accept': 'application/json'};
-    var uri = Uri.parse('https://api.covid19api.com/summary');
-    try {
-      var response = await http.get(uri, headers: headers1);
-      if (response.statusCode == 200) {
-        return response.body;
-      } else {
-        return 'Service Temporarily Unavailable';
-      }
-    } catch (e) {
-      return 'Something went wrong';
-    }
-  }
-
-  static Future<String> getUser() async {
-    Map<String, String> headers1 = {'Accept': 'application/json'};
-    var uri = Uri.parse('https://jsonplaceholder.typicode.com/posts');
-    try {
-      var response = await http.get(uri, headers: headers1);
-      if (response.statusCode == 200) {
-        return response.body;
-      } else {
-        return "400";
-      }
-    } catch (e) {
-      return "500";
-    }
-  }
-
-  static Future<String> getUserProfile() async {
-    Map<String, String> headers1 = {'Accept': 'application/json'};
-    var uri = Uri.parse('https://jsonplaceholder.typicode.com/users');
-    try {
-      var response = await http.get(uri, headers: headers1);
-      if (response.statusCode == 200)
-        return response.body;
-      else
-        return '400';
-    } catch (e) {
-      return '500';
-    }
-  }
-
-  static Future<dynamic> getCountries() async {
-    Map<String, String> headers = {'Accept': 'application/json'};
-    var uri = Uri.parse('https://api.covid19api.com/countries');
-    try {
-      var response = await http.get(uri, headers: headers);
-      return response;
-    } catch (e) {
-      return e;
-    }
-  }
 
   static Future<dynamic> getCountriesCases() async {
     Map<String, String> headers = {'Accept': 'application/json'};
