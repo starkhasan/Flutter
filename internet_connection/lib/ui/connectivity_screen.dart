1d23d66025b678adffeabd61491ca97d75ba2aad
starkhasan <alihasan226@gmail.com>
2021-08-19 18:30:19 +0530

    Modify UI of the NoInternetConnection

--- internet_connection/lib/ui/connectivity_screen.dart
+++ internet_connection/lib/ui/connectivity_screen.dart
@@ -3,6 +3,7 @@ import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:internet_connection/provider/connectivity_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:lottie/lottie.dart';
 
 class ConnectivityScreen extends StatelessWidget {
   const ConnectivityScreen({ Key? key }) : super(key: key);
@@ -15,9 +16,35 @@ class ConnectivityScreen extends StatelessWidget {
       child: Consumer<ConnectivityResult>(
         builder: (context,provider,child){
           checkConnectivity(provider,context);
-          return const Scaffold(
-            body: Center(
-              child: Text('Internet Connectivity')
+          return Scaffold(
+            body: Stack(
+              children: [
+                Positioned(
+                  left: MediaQuery.of(context).size.width * 0.05,
+                  top: kToolbarHeight,
+                  child: InkWell(
+                    onTap: () => Navigator.pop(context),
+                    child: const Icon(Icons.arrow_back_ios_new_rounded),
+                  )
+                ),
+                Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Lottie.asset('assets/lotties/connectivity.json',height: MediaQuery.of(context).size.width * 0.30,width: MediaQuery.of(context).size.width * 0.30),
+                      const Text('No Internet Connection',style: TextStyle(color: Colors.black,fontSize: 18)),
+                      const SizedBox(height: 5),
+                      const Flexible(child: Text('Please check your internet connection and try again')),
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () => checkConnectivity(provider, context),
+                        child: const Text('Retry')
+                      )
+                    ]
+                  )
+                )
+              ]
             )
           );
         }
@@ -28,9 +55,6 @@ class ConnectivityScreen extends StatelessWidget {
   void checkConnectivity(ConnectivityResult result,BuildContext context){
     if(result == ConnectivityResult.wifi || result == ConnectivityResult.mobile){
       Timer(const Duration(seconds: 2), () => Navigator.pop(context));
-    }else{
-      var snackBar = const SnackBar(content: Text('Try Again!',style: TextStyle(color: Colors.red)),duration: Duration(seconds: 1));
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
     }
   }
 }
