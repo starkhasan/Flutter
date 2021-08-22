9f76eb7c1f2594171978bd33f40fcf93b42a4df4
starkhasan <alihasan226@gmail.com>
2021-08-22 19:14:26 +0530

    Modify UI of the SplashScreen

--- fingerprint_auth/lib/ui/splash_screen.dart
+++ fingerprint_auth/lib/ui/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:fingerprint_auth/ui/login_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -9,13 +8,53 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     startTimer(context);
-    return const Scaffold(
-      body: Center(
-        child: Text('Splash Screen')
-      ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/backLogin.jpg'),
+                fit: BoxFit.cover
+              )
+            )
+          ),
+          Positioned(
+            bottom: MediaQuery.of(context).size.height * 0.15,
+            child: Container(
+              padding: EdgeInsets.zero,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'NetGym',
+                    style: TextStyle(fontSize: 45.0,fontWeight: FontWeight.bold,color: Colors.white)
+                  ),
+                  const SizedBox(height: 2),
+                  RichText(
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(text: 'Addicted to getting',style: TextStyle(color: Colors.white)),
+                        TextSpan(text: ' STRONG',style: TextStyle(color: Colors.red))
+                      ]
+                    )
+                  )
+                ]
+              )
+            )
+          )
+        ]
+      )
     );
   }
 
+  void startTimer(BuildContext context) {
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.pushReplacement(context,
+      MaterialPageRoute(builder: (context) => const LoginScreen()))
+    );
+  }
 
-  void startTimer(BuildContext context) => Timer(const Duration(seconds:  2), () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const LoginScreen())));
 }
