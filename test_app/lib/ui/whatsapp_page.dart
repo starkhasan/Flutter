5ac625bd738e3ffe2d61bfac9cb9a669aeb99803
starkhasan <alihasan226@gmail.com>
2021-10-08 10:17:01 +0530

    Modify WhatsAppPage

--- test_app/lib/ui/whatsapp_page.dart
+++ test_app/lib/ui/whatsapp_page.dart
@@ -10,52 +10,35 @@ class WhatsAppPage extends StatefulWidget {
 class _WhatsAppPageState extends State<WhatsAppPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            automaticallyImplyLeading: false,
-            actions: [
-              IconButton(
-                onPressed: () => print('Search'),
-                icon: const Icon(Icons.search)
-              ),
-              IconButton(
-                onPressed: () => print('More'),
-                icon: const Icon(Icons.more_vert)
-              )
-            ],
-            centerTitle: false,
-            title: const Text('WhatsApp')
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate([mainBody()])
-          )
-        ]
-      )
-    );
-  }
-
-  Widget mainBody(){
     return DefaultTabController(
-      length: 3,
+      length: 3, 
       child: Scaffold(
-        appBar: AppBar(
-          bottom: const TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.directions_car)),
-              Tab(icon: Icon(Icons.directions_transit)),
-              Tab(icon: Icon(Icons.directions_bike)),
-            ]
-          )
-        ),
-        body: const TabBarView(
-          children: [
-            Icon(Icons.directions_car),
-            Icon(Icons.directions_transit),
-            Icon(Icons.directions_bike),
+        body: CustomScrollView(
+          slivers: [
+            const SliverAppBar(
+              floating: true,
+              title: Text('WhatsApp'),
+              bottom: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.directions_car)),
+                  Tab(icon: Icon(Icons.directions_transit)),
+                  Tab(icon: Icon(Icons.directions_bike))
+                ]
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate([
+                  const TabBarView(
+                    children: [
+                      Icon(Icons.directions_car),
+                      Icon(Icons.directions_transit),
+                      Icon(Icons.directions_bike)
+                    ]
+                  )
+              ])
+            )
           ],
-        )
+        ),
       )
     );
   }
