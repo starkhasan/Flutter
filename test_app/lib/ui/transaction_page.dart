7527df0961e1ff9cc675bd0bc8d3dd3f100bea59
starkhasan <alihasan226@gmail.com>
2021-10-09 20:29:29 +0530

    Add PageView For ContentSwipe

--- test_app/lib/ui/transaction_page.dart
+++ test_app/lib/ui/transaction_page.dart
@@ -9,6 +9,34 @@ class TransactionPage extends StatefulWidget {
 }
 
 class _TransactionPageState extends State<TransactionPage> {
+
+  late PageController pageController;
+  var currentTransactionPage = 0;
+
+  @override
+  void initState() {
+    pageController = PageController(
+      initialPage: 0
+    );
+    pageController.addListener(pageControllerListener);
+    super.initState();
+  }
+
+  pageControllerListener(){
+    if(pageController.page == 0.0){
+      setState(() {currentTransactionPage = 0;});
+    }
+    if(pageController.page == 1.0){
+      setState(() {currentTransactionPage = 1;});
+    }
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +56,7 @@ class _TransactionPageState extends State<TransactionPage> {
               leading: IconButton(onPressed: () => Navigator.pop(context),icon: const Icon(Icons.arrow_back,color: Colors.black)),
               actions: [
                 IconButton(
-                  onPressed: () => print('Click on Search Icon'),
+                  onPressed: () => showSnackBar(context,'Click on Search Icon'),
                   icon: const Icon(Icons.search,color: Colors.grey))
               ],
             ),
@@ -54,34 +82,78 @@ class _TransactionPageState extends State<TransactionPage> {
               children: [
                 const SizedBox(height: 20),
                 const Text('Transaction',style: TextStyle(color: Colors.black,fontSize: 28,fontWeight: FontWeight.bold)),
-                const SizedBox(height: 20),
-                GridView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: 8,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: (MediaQuery.of(context).orientation == Orientation.landscape) ? 2 : 3),
-                  itemBuilder: (BuildContext context,int index){
-                    return Container(
-                      margin: const EdgeInsets.only(left: 5,right: 5,bottom: 10),
-                      padding: const EdgeInsets.all(15),
-                      height: 150.0,
-                      decoration: BoxDecoration(
-                        color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
-                        borderRadius: BorderRadius.circular(15)
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('${73+index}',style: const TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold)),
-                          const Text('Waiting for Confirmation')
-                        ]
-                      ),
-                    );
-                  }
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text('Launched',style: TextStyle(color: currentTransactionPage == 0 ? Colors.black : Colors.grey[400],fontSize: currentTransactionPage == 0 ? 22 : 18,fontWeight: FontWeight.bold)),
+                    const SizedBox(width: 30),
+                    Text('Received',style: TextStyle(color: currentTransactionPage == 1 ? Colors.black : Colors.grey[400],fontSize: currentTransactionPage == 1 ? 22 : 18,fontWeight: FontWeight.bold))
+                  ]
                 ),
                 const SizedBox(height: 20),
-                const Text('Tender Transaction',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 18))
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.52,
+                  padding: EdgeInsets.zero,
+                  child: PageView(
+                    controller: pageController,
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: 8,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: (MediaQuery.of(context).orientation == Orientation.landscape) ? 2 : 3),
+                        itemBuilder: (BuildContext context,int index){
+                          return Container(
+                            margin: const EdgeInsets.only(left: 5,right: 5,bottom: 10),
+                            padding: const EdgeInsets.all(15),
+                            height: MediaQuery.of(context).size.height * 0.10,
+                            decoration: BoxDecoration(
+                              color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+                              borderRadius: BorderRadius.circular(15)
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text('${73+index*2}',style: const TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold)),
+                                const Text('Waiting for Confirmation')
+                              ]
+                            ),
+                          );
+                        }
+                      ),
+                      GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: 8,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: (MediaQuery.of(context).orientation == Orientation.landscape) ? 2 : 3),
+                        itemBuilder: (BuildContext context,int index){
+                          return Container(
+                            margin: const EdgeInsets.only(left: 5,right: 5,bottom: 10),
+                            padding: const EdgeInsets.all(15),
+                            height: MediaQuery.of(context).size.height * 0.10,
+                            decoration: BoxDecoration(
+                              color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+                              borderRadius: BorderRadius.circular(15)
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text('${1+index*2}',style: const TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold)),
+                                const Text('In Progress')
+                              ]
+                            ),
+                          );
+                        }
+                      ),
+                    ]
+                  )
+                ),
+                const SizedBox(height: 20),
+                const Text('New Transactions',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20))
               ]
             )
           ),
@@ -110,7 +182,7 @@ class _TransactionPageState extends State<TransactionPage> {
                   ] 
                 ),
                 GestureDetector(
-                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const TransactionPage())),
+                  onTap: () => showSnackBar(context, 'Pairing'),
                   child: Container(
                     padding: const EdgeInsets.only(left: 30,right: 30,top: 14,bottom: 14),
                     decoration: BoxDecoration(
@@ -131,7 +203,7 @@ class _TransactionPageState extends State<TransactionPage> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 GestureDetector(
-                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const TransactionPage())),
+                  onTap: () => showSnackBar(context, 'Deleted'),
                   child: Container(
                     padding: const EdgeInsets.only(left: 30,right: 30,top: 14,bottom: 14),
                     decoration: BoxDecoration(
@@ -144,7 +216,7 @@ class _TransactionPageState extends State<TransactionPage> {
                 ),
                 const SizedBox(width: 15),
                 GestureDetector(
-                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const TransactionPage())),
+                  onTap: () => showSnackBar(context, 'Accepted'),
                   child: Container(
                     padding: const EdgeInsets.only(left: 30,right: 30,top: 14,bottom: 14),
                     decoration: BoxDecoration(
@@ -179,7 +251,7 @@ class _TransactionPageState extends State<TransactionPage> {
                   ] 
                 ),
                 GestureDetector(
-                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const TransactionPage())),
+                  onTap: () => showSnackBar(context, 'Pairing'),
                   child: Container(
                     padding: const EdgeInsets.only(left: 30,right: 30,top: 14,bottom: 14),
                     decoration: BoxDecoration(
@@ -193,8 +265,15 @@ class _TransactionPageState extends State<TransactionPage> {
             ),
           ),
           const SizedBox(height: 30),
+          const SizedBox(height: 30),
         ]
       )
     );
   }
+
+  showSnackBar(BuildContext _context,String message){
+    var snackbar = SnackBar(backgroundColor: Colors.grey[200],content: Text(message),duration: const Duration(seconds: 1));
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
 }
