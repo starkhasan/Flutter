782a6dc4f3ff8a9e7d163a740f1f5b00b27566fc
starkhasan <alihasan226@gmail.com>
2021-10-10 20:29:18 +0530

    Add Flow Widget in Flutter

--- test_app/lib/ui/webview_page.dart
+++ test_app/lib/ui/webview_page.dart
@@ -11,32 +11,108 @@ class WebViewPage extends StatefulWidget {
   _WebViewPageState createState() => _WebViewPageState();
 }
 
-class _WebViewPageState extends State<WebViewPage> {
+class _WebViewPageState extends State<WebViewPage> with SingleTickerProviderStateMixin{
   final Completer<WebViewController> _controller = Completer<WebViewController>();
+
+  final List<IconData> menuItems = [Icons.add,Icons.home,Icons.notification_add,Icons.settings];
+  late AnimationController menuAnimation;
+  IconData lastTapped = Icons.notifications;
+
   @override
   void initState() {
     super.initState();
     if (Platform.isAndroid) {
       WebView.platform = SurfaceAndroidWebView();
     }
+    menuAnimation = AnimationController(duration: const Duration(milliseconds: 250), vsync: this);
   }
+
+  Widget flowMenuItem(IconData icon) {
+    return GestureDetector(
+      onTap: (){
+        updateMenu(icon);
+          menuAnimation.status == AnimationStatus.completed
+              ? menuAnimation.reverse()
+              : menuAnimation.forward();
+      },
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        margin: const EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          color: lastTapped == icon ? Colors.amber[700] : Colors.blue,
+          shape: BoxShape.circle
+        ),
+        child: Icon(
+          icon,
+          color: Colors.white,
+          size: 35.0,
+        )
+      )
+    );
+  }
+
+  updateMenu(IconData icon) {
+    if (icon != Icons.menu) {
+      setState(() => lastTapped = icon);
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: const Text('WebView Page'),actions: [ NavigationController(webViewController: _controller.future)]),
-      body: Container(
-        padding: const EdgeInsets.all(0),
-        child: WebView(
-          initialUrl: 'https://www.google.co.in/',
-          javascriptMode: JavascriptMode.unrestricted,
-          gestureNavigationEnabled: true,
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller.complete(webViewController);
-          },
-        )
-      ),
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.red,
+            child: WebView(
+              initialUrl: 'https://www.google.co.in/',
+              javascriptMode: JavascriptMode.unrestricted,
+              gestureNavigationEnabled: true,
+              onWebViewCreated: (WebViewController webViewController) {
+                _controller.complete(webViewController);
+              },
+            )
+          ),
+          Flow(
+            delegate: FlowMenuDelegate(menuAnimation: menuAnimation,parentContext: context),
+            children: menuItems.map<Widget>((IconData icon) => flowMenuItem(icon)).toList(),
+          )
+        ]
+      )
     );
+  }
+}
+
+class FlowMenuDelegate extends FlowDelegate {
+  FlowMenuDelegate({required this.menuAnimation,required this.parentContext})
+      : super(repaint: menuAnimation);
+
+  final Animation<double> menuAnimation;
+  final BuildContext parentContext;
+
+  @override
+  bool shouldRepaint(FlowMenuDelegate oldDelegate) {
+    return menuAnimation != oldDelegate.menuAnimation;
+  }
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    double dy = 0.0;
+    double horizontalAxis = MediaQuery.of(parentContext).size.width * 0.80;
+    double verticalAxis = MediaQuery.of(parentContext).size.height * 0.80;
+    for (int i = 0; i < context.childCount; ++i) {
+      dy = context.getChildSize(i)!.width * (-i);
+      context.paintChild(
+        i,
+        transform: Matrix4.translationValues(
+          horizontalAxis,
+          dy * menuAnimation.value + verticalAxis,
+          0,
+        ),
+      );
+    }
   }
 }
 
