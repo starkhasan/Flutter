09b8e35adf777862c6bfd9bb0cc578fc7738a3d3
starkhasan <alihasan226@gmail.com>
2021-10-13 01:17:00 +0530

    Add AppIcon and Modify SplashScreen

--- notes_todo/lib/view/splash_screen.dart
+++ notes_todo/lib/view/splash_screen.dart
@@ -13,12 +13,46 @@ class SplashScreen extends StatelessWidget {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]);
     Preferences.init();
     startTimer(context);
-    return const Scaffold(
-      body: Center( child: Text('Splash Screen',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.black))),
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset('assets/logo.png',height: MediaQuery.of(context).size.height * 0.12, width: MediaQuery.of(context).size.height * 0.12),
+                  const Text(
+                    'Notes Todo',
+                    style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold,fontFamily: '')
+                  )
+                ]
+              )
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 0, MediaQuery.of(context).size.height * 0.10),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Design & Developed by : Traversal',
+                      style: TextStyle(color: Colors.grey[300],fontSize: 12)
+                    )
+                  ]
+                )
+              )
+            )
+          ]
+        )
+      )
     );
   }
 
   startTimer(BuildContext _context) {
-    Timer(const Duration(seconds: 2),() => Navigator.pushReplacement(_context, MaterialPageRoute(builder: (context) => const NotesPage())));
+    Timer(const Duration(seconds: 3),() => Navigator.pushReplacement(_context, MaterialPageRoute(builder: (context) => const NotesPage())));
   }
 }
