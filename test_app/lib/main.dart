d47cee21fcd8b8fb540fecf4ad62af0cdcbfb6f1
starkhasan <alihasan226@gmail.com>
2021-10-27 11:21:47 +0530

    Fixed Issue of the main.dart

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -39,11 +39,11 @@ void main() async {
     badge: true,
     sound: true,
   );
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
