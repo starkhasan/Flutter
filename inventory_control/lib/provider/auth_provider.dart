fcbf845c2a862bd2c6f91370e1d033930bd730c3
starkhasan <alihasan226@gmail.com>
2021-11-06 14:00:05 +0530

    Refactor Code

--- inventory_control/lib/provider/auth_provider.dart
+++ inventory_control/lib/provider/auth_provider.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:inventory_control/utils/helper.dart';
