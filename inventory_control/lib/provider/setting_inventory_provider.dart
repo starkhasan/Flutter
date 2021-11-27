cb9a1861152c34e1157c5bc5378015861e223f4e
starkhasan <alihasan226@gmail.com>
2021-11-28 01:23:30 +0530

    Implement Rename of Inventory

--- inventory_control/lib/provider/setting_inventory_provider.dart
+++ inventory_control/lib/provider/setting_inventory_provider.dart
@@ -7,6 +7,7 @@ import 'package:inventory_control/utils/preferences.dart';
 class SettingInventoryProvider extends ChangeNotifier with Helper{
   bool _isLoading = false;
   bool get loading => _isLoading;
+  List<String> allInventory = [];
 
   var firebaseReference = FirebaseDatabase.instance.reference().child('inventory_control').child(Preferences.getUserId());
   var settingInventoryData = SettingInventoryModel('2021-11-08 11:23:08.316419', false,0,0,0,0,0);
@@ -15,6 +16,17 @@ class SettingInventoryProvider extends ChangeNotifier with Helper{
     if(loading){
       _isLoading = true;
       notifyListeners();
+    }
+    if(allInventory.isEmpty){
+      await firebaseReference.once().then((snapshot){
+        if(snapshot.value != null){
+          snapshot.value.keys.forEach((item) {
+            if(item != 'email' && item != 'userName' && item != 'createdAt' && item != 'profileImage' && item != inventoryName){
+              allInventory.add(item);
+            }
+          });
+        }
+      });
     }
     await firebaseReference.child(inventoryName).once().then((DataSnapshot snapshot) {
       if(snapshot.value != null){
@@ -30,7 +42,9 @@ class SettingInventoryProvider extends ChangeNotifier with Helper{
           settingInventoryData.totalProduct = snapshot.value['inventory'].length;
           var inStock = 0;
           var outStock = 0;
+          allInventory.clear();
           for(var item in snapshot.value['inventory'].keys){
+            allInventory.add(item);
             if(snapshot.value['inventory'][item]['quantity'] > 0){
               inStock+=1;
             }else{
@@ -47,19 +61,27 @@ class SettingInventoryProvider extends ChangeNotifier with Helper{
   }
 
   renameInventoryName(BuildContext _context,String oldName,String newName) async{
-    _isLoading = true;
-    notifyListeners();
-    var oldData = <String,dynamic>{};
-    await firebaseReference.child(oldName).once().then((DataSnapshot snapshot) {
-      if(snapshot.value != null){
-        oldData = Map<String,dynamic>.from(snapshot.value);
-      }
-    });
-    await firebaseReference.child(newName).update(oldData);
-    await firebaseReference.child(oldName).remove();
-    if(oldName == Preferences.getInventoryName()) Preferences.setInventoryName(newName);
-    showSnackBar(_context, 'Inventory name updated successfully');
-    await getInventoryData(false,_context, newName);
+    if(newName.isEmpty){
+      showSnackBar(_context, 'Please provide inventory name');
+    }else if(allInventory.contains(newName)){
+      showSnackBar(_context, 'Name Already Exist');
+    }else if(oldName == newName){
+      showSnackBar(_context, 'Inventory name updated successfully');
+    }else{
+      _isLoading = true;
+      notifyListeners();
+      var oldData = <String,dynamic>{};
+      await firebaseReference.child(oldName).once().then((DataSnapshot snapshot) {
+        if(snapshot.value != null){
+          oldData = Map<String,dynamic>.from(snapshot.value);
+        }
+      });
+      await firebaseReference.child(newName).update(oldData);
+      await firebaseReference.child(oldName).remove();
+      if(oldName == Preferences.getInventoryName()) Preferences.setInventoryName(newName);
+      showSnackBar(_context, 'Inventory name updated successfully');
+      await getInventoryData(false,_context, newName);
+    }
   }
 
   enabledInventory(BuildContext context,String inventoryName,bool value) async{
