92d9c2c56b5d8764fe26e1952981178fd2befd61
starkhasan <alihasan226@gmail.com>
2021-12-02 17:27:02 +0530

    Implement Attach Image

--- test_app/lib/ui/grocery/rating_screen.dart
+++ test_app/lib/ui/grocery/rating_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:image_picker/image_picker.dart';
 
 class RatingScreen extends StatefulWidget {
   final String productId;
@@ -16,6 +20,7 @@ class _RatingScreenState extends State<RatingScreen> {
   var newRating = 0.0;
   var titleController = TextEditingController();
   var reviewController = TextEditingController();
+  var imagePath = '';
 
   @override
   Widget build(BuildContext context) {
@@ -88,33 +93,98 @@ class _RatingScreenState extends State<RatingScreen> {
             decoration: const InputDecoration(hintText: 'Write your experience',hintStyle: TextStyle(color: Colors.grey)),
           ),
           const SizedBox(height: 10),
-          InkWell(
-            onTap: () => print('Click Here to select image file'),
-            child: Container(
-              margin: const EdgeInsets.all(15),
-              padding: const EdgeInsets.only(top: 12,bottom: 12),
-              decoration: BoxDecoration(color: Colors.green[100],borderRadius: const BorderRadius.all(Radius.circular(5))),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(Icons.attach_file,size: 20),
-                  SizedBox(height: 5),
-                  Text('Attach Photo',style: TextStyle(fontWeight: FontWeight.bold))
-                ]
+          imagePath.isEmpty
+           ? InkWell(
+              onTap: ()  async {
+                var source = await chooseImageSource();
+                uploadImageFile(context,source);
+              },
+              child: Container(
+                margin: const EdgeInsets.only(top: 15,left: 15,right: 15),
+                padding: const EdgeInsets.only(top: 12,bottom: 12),
+                decoration: BoxDecoration(color: Colors.green[100],borderRadius: const BorderRadius.all(Radius.circular(5))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.attach_file,size: 20),
+                    SizedBox(height: 5),
+                    Text('Attach Photo',style: TextStyle(fontWeight: FontWeight.bold))
+                  ]
+                )
               )
+            )
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Attached Photo',style: TextStyle(fontSize: 16,color: Colors.black,fontWeight: FontWeight.bold)),
+                const SizedBox(height: 5),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5,right: 15,top: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: Image.file(File(imagePath)),
+                      ),
+                      InkWell(
+                        onTap: () => setState(() => imagePath = ''),
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(color: Colors.red[50],border: Border.all(width: 1.0,color: Colors.red),borderRadius: const BorderRadius.all(Radius.circular(20.0))),
+                          child: const Text('Remove',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.red))
+                        ),
+                      )
+                    ]
+                  ),
+                )
+              ]
             ),
-          ),
           InkWell(
             onTap: () => print('Click Here to Publish Review'),
             child: Container(
-              margin: const EdgeInsets.only(left: 15,right: 15,bottom: 15),
+              margin: const EdgeInsets.only(top: 15,left: 15,right: 15,bottom: 15),
               padding: const EdgeInsets.only(top: 15,bottom: 15),
               decoration: const BoxDecoration(color: Colors.green,borderRadius: BorderRadius.all(Radius.circular(5))),
               child: const Center(child: Text('Publish Review',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold)))
-            ),
-          ),
+            )
+          )
         ]
       )
     );
+  }
+
+  Future<ImageSource> chooseImageSource() async{
+    var source = await showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context){
+        return CupertinoAlertDialog(
+          title: const Text('Choose Image Source',style: TextStyle(fontSize: 14)),
+          actions: [
+            CupertinoDialogAction(
+              onPressed: () => Navigator.pop(context,ImageSource.camera),
+              child: const Text('Camera',style: TextStyle(fontSize: 14))
+            ),
+            CupertinoDialogAction(
+              onPressed: () => Navigator.pop(context, ImageSource.gallery),
+              child: const Text('Gallery',style: TextStyle(color: Colors.red,fontSize: 14))
+            )
+          ]
+        );
+      }
+    );
+    return source;
+  }
+
+  Future<void> uploadImageFile(BuildContext _context,ImageSource imgSource) async{
+    var photo = await ImagePicker().pickImage(source: imgSource);
+    if(photo!=null){
+      setState(() {
+        imagePath = photo.path;
+      });
+    }
   }
 }
