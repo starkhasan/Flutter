d371df8b5f316998e3a203ccb5d44df3e311f7dc
starkhasan <alihasan226@gmail.com>
2021-12-13 01:23:39 +0530

    Implement Music Player Animation

---
+++ block_arch/lib/dart_check.dart
@@ -0,0 +1,6 @@
+import 'dart:math';
+
+void main() {
+  var number = Random().nextDouble() * 50.0;
+  print(number);
+}
--- block_arch/lib/ui/animation/tween_explicit.dart
+++ block_arch/lib/ui/animation/tween_explicit.dart
@@ -4,47 +4,92 @@ class TweenExplicit extends StatefulWidget {
   const TweenExplicit({ Key? key }) : super(key: key);
 
   @override
-  _TweenExplicitState createState() => _TweenExplicitState();
+  State<TweenExplicit> createState() => _TweenExplicitState();
 }
 
-class _TweenExplicitState extends State<TweenExplicit> with SingleTickerProviderStateMixin{
-  
-  late AnimationController _controller;
-  late Animation<double> animation;
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 5000));
-    animation = Tween(begin: 0.0, end: 300.0).animate(_controller);
-    _controller.forward();
+class _TweenExplicitState extends State<TweenExplicit> {
 
-    _controller.repeat(reverse: true);
-  }
+  final List<Duration> animationDuration = const [Duration(milliseconds: 400),Duration(milliseconds: 300),Duration(milliseconds: 500),Duration(milliseconds: 700)];
+
+  bool isAnimate = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Title")),
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return Row(
-              children: [
-                Transform.translate(
-                  child: Container(width: 100, height: 100, color: Colors.black),
-                  offset: Offset(animation.value, 0),
-                ),
-                Transform.translate(
-                  child: Container(width: 100, height: 100, color: Colors.red),
-                  offset: Offset(animation.value, 0),
-                )
-              ]
-            );
-          }
-        )
+      appBar: AppBar(title: const Text("Music Play Explicit Animation")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print('Click Here'),
+        child: const Icon(Icons.stop,),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: MediaQuery.of(context).size.height * 0.30),
+          SizedBox(
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: List.generate(4, (index) => MainScreen(durations: animationDuration[index])),
+            )
+          ),
+          Expanded(
+            child: Container()
+          )
+        ]
       )
     );
   }
+}
 
+
+class MainScreen extends StatefulWidget {
+  final Duration durations;
+  const MainScreen({ Key? key,required this.durations}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin{
+  late AnimationController animationController;
+  late Animation<double> animation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: widget.durations,
+      lowerBound: 5,
+      upperBound: 100
+    );    
+    // final curveAnimation = CurvedAnimation(parent: animationController, curve: Curves.linear);
+    // animation = Tween<double>(begin: 0, end: 100).animate(curveAnimation);
+    animationController.addListener(() => setState(() {}));
+    animationController.repeat(reverse: true);
+  }
+
+  bool changeAnimationController(String type) {
+    if(type == 'stop'){
+      animationController.reset();
+      return false;
+    }else{
+      animationController.repeat(reverse: true);
+      return true;
+    }
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose(); 
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 1,top: animationController.value),
+      width: 15,
+      decoration: const BoxDecoration(color: Colors.black,borderRadius: BorderRadius.only(topLeft: Radius.circular(1.0),topRight: Radius.circular(1.0))),
+    );
+  }
 }
