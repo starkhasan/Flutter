2f3db316c7a3fc670356843fbd77481b770c0d77
starkhasan <alihasan226@gmail.com>
2022-01-18 23:41:39 +0530

    Remove Grocery Project

--- test_app/lib/utils/helper.dart
+++ test_app/lib/utils/helper.dart
@@ -3,7 +3,6 @@ import 'package:test_app/ui/deeplink/platform_integration_page.dart';
 import 'package:test_app/ui/facebook_page.dart';
 import 'package:test_app/ui/crypto_page.dart';
 import 'package:test_app/ui/get_started/get_started_home.dart';
-import 'package:test_app/ui/grocery/main_screen.dart';
 import 'package:test_app/ui/isolates_example.dart';
 import 'package:test_app/ui/keys_example/scroll_position_example.dart';
 import 'package:test_app/ui/keys_example/widget_element_keys.dart';
@@ -24,32 +23,6 @@ class Helper {
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
-  static List<String> listImageURL = [
-    'https://m.media-amazon.com/images/I/91pDdDLHquL._SX522_.jpg',
-    'https://www.bigbasket.com/media/uploads/p/xxl/40198145_1-popular-essentials-premium-jeera-rice.jpg',
-    'https://m.media-amazon.com/images/I/71LpBnx+5xL._SL1500_.jpg',
-    'https://m.media-amazon.com/images/I/71bSLxCaGGL._SL1500_.jpg',
-    'https://www.jiomart.com/images/product/original/490001392/amul-butter-500-g-carton-6-20210315.jpg',
-    'https://www.bigbasket.com/media/uploads/p/l/104823_3-amul-cheese-spread-yummy-plain.jpg',
-    'https://5.imimg.com/data5/PF/FT/XN/SELLER-6800096/amul-gold-milk-500ml-500x500.jpg',
-    'https://m.media-amazon.com/images/I/615etm93zBL._SX522_.jpg',
-    'https://m.media-amazon.com/images/I/519YSKro-XL.jpg',
-    'https://m.media-amazon.com/images/I/61L0w87gCML._SX522_.jpg',
-  ];
-
-  static List<String> listProductName = [
-    'Flour',
-    'Rice',
-    'Sugar',
-    'Salt',
-    'Butter',
-    'Cheese',
-    'Milk',
-    'Red Chilli Powder',
-    'Turmeric Powder',
-    'Coriander Powder' 
-  ];
-
   static var screenNames = [
     'Facebook',
     'Twitter',
@@ -65,7 +38,6 @@ class Helper {
     'Widget Element Keys',
     'Scroll Position',
     'Video Player',
-    'Grocery Plus',
     'Learning Isolates',
     'Get Started With Flutter'
   ];
@@ -95,18 +67,16 @@ class Helper {
             context, MaterialPageRoute(builder: (context) => const Crypto()));
         break;
       case 4:
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => const WebViewPage()));
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const WebViewPage()));
         break;
       case 5:
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => const SliverWidgetPage()));
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const SliverWidgetPage()));
         break;
       case 6:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const FirebaseAuthentication()));
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const FirebaseAuthentication()));
         break;
       case 7:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const RazorpayPaymentGateway()));
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const RazorpayPaymentGateway()));
         break;
       case 8:
         Navigator.push(
@@ -119,10 +89,10 @@ class Helper {
         ).then((value) => {});
         break;
       case 9:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const RestAPI()));
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const RestAPI()));
         break;
       case 10:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const WebSocket()));
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const WebSocket()));
         break;
       case 11:
         Navigator.push(context,MaterialPageRoute(builder: (context) => const WidgetElementKeys()));
@@ -132,9 +102,6 @@ class Helper {
         break;
       case 13:
         Navigator.push(context,MaterialPageRoute(builder: (context) => const VideoPlayScreen()));
-        break;
-      case 14:
-        Navigator.push(context,MaterialPageRoute(builder: (context) => const MainScreen()));
         break;
       case 15:
         Navigator.push(context,MaterialPageRoute(builder: (context) => const IsolateExample()));
